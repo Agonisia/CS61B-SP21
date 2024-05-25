8468d6c311d137adc034eeb2665a6e0763ad800d
Peiz Li <agnosia25050116@gmail.com>
2024-05-26 01:29:09 +0800

    complete Lab 7 draft ver.1

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,250 @@
+package bstmap;
+
+
+import java.util.*;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private Node root;
+    private int size;
+
+    private class Node {
+        K key;
+        V value;
+        Node left, right;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node.value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<>();
+        keySet(root, set);
+        return set;
+    }
+
+    private void keySet(Node node, Set<K> set) {
+        if (node == null) {
+            return;
+        }
+
+        set.add(node.key);
+        keySet(node.left, set);
+        keySet(node.right, set);
+    }
+
+    @Override
+    public V remove(K key) {
+        // throw new UnsupportedOperationException("remove not supported");
+        Node nodeToRemove = getNode(root, key);
+        if (nodeToRemove == null) {
+            return null;
+        }
+        V value = nodeToRemove.value;
+        root = remove(root, key);
+        return value;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        // throw new UnsupportedOperationException("remove not supported");
+        Node nodeToRemove = getNode(root, key);
+        if (nodeToRemove == null || !nodeToRemove.value.equals(value)) {
+            return null;
+        }
+        root = remove(root, key);
+        return value;
+    }
+
+    private Node getNode(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return getNode(node.left, key);
+        } else if (cmp > 0) {
+            return getNode(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    private Node remove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = remove(node.left, key);
+        } else if (cmp > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            Node temp = node;
+            node = min(temp.right);
+            node.right = removeMin(temp.right);
+            node.left = temp.left;
+        }
+        return node;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        } else {
+            return min(node.left);
+        }
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+
+    @Override
+    public Iterator<K> iterator() {
+        return new BSTMapIterator();
+    }
+
+    private class BSTMapIterator implements Iterator<K> {
+        private Stack<Node> stack = new Stack<>();
+
+        public BSTMapIterator() {
+            pushLeft(root);
+        }
+
+        private void pushLeft(Node node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Node node = stack.pop();
+            pushLeft(node.right);
+            return node.key;
+        }
+    }
+
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    private void printInOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.left);
+        System.out.println(node.key + " => " + node.value);
+        printInOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+        BSTMap<Integer, String> bst = new BSTMap<>();
+        bst.put(3, "Three");
+        bst.put(1, "One");
+        bst.put(4, "Four");
+        bst.put(2, "Two");
+
+        bst.printInOrder();
+        System.out.println("Removing key 3");
+        bst.remove(3);
+        bst.printInOrder();
+
+        System.out.println("Removing key 1 with value 'One'");
+        bst.remove(1, "One");
+        bst.printInOrder();
+
+        System.out.println("Removing key 4 with value 'Five' (should not remove)");
+        bst.remove(4, "Five");
+        bst.printInOrder();
+    }
+
+}
